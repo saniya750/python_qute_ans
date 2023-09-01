8b67c6252350835a212e71493a6035ae6034ca86
saniya750 <sayedsaniya16@.com>
2023-09-01 21:56:58 +0530

    python interview question completed

---
+++ python_qt_ans.py
@@ -0,0 +1,143 @@
+a = ("a", "b", "c", "d", "e", "f", "g", "h","i","j","k","l","m")
+print(a[::-1])
+#output:('m', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a')
+print(a[2:5])
+#output:('c', 'd', 'e')
+
+#output:('m', 'l', 'k', 'j', 'i', 'h', 'g')
+#output:('h',)
+#output:('h', 'g', 'f', 'e')
+#output:('a', 'c', 'e', 'g', 'i', 'k', 'm')
+#output:('m', 'k', 'i', 'g', 'e', 'c', 'a')
+#output:('m', 'j', 'g', 'd')
+
+str="Characters"
+#output:sretcarahC
+#output:ter
+#output:racte
+#output:rces
+
+text1="my name is john and i m from london"
+#o/p=['my', 'name', 'is', 'john', 'and', 'i', 'm','from', 'london']
+
+text2 = "abc$xyz$123"
+#o/p=['abc', 'xyz', '123']
+
+
+
+#Q. Write a program to find biggest of given 2 numbers from the commad prompt?
+#Q. Write a program to find biggest of given 3 numbers from the commad prompt?
+#Q. Write a program to find smallest of given 2 numbers?
+#Q. Write a program to find smallest of given 3 numbers?
+#Q. Write a program to check whether the given number is even or odd?
+#Q. Write a program to check whether the given number is in between 1 and 100?
+#Eg 2:To print characters present in string index wise
+s="sumayya"
+i=0,j=s
+i=1,j=u
+i=2,j=m
+i=3,j=a
+i=4,j=y
+#Eg 3: To print Hello 30 times
+#Eg 4: To display numbers from 0 to 10
+#Eg 5: To display odd numbers from 0 to 20
+#Eg 6: To display numbers from 10 to 1 in descending order
+#Eg 7: To print sum of numbers presenst inside list
+#Eg: To print numbers from 1 to 10 by using while loop
+#Eg: To display the sum of first n numbers
+#Eg: write a program to prompt user to enter some name until entering Sumayya
+#Q. What is the difference between for loop and while loop in Python?
+#Q. How to exit from the loop ?
+#Q. How to skip some iterations inside loop?
+#Q. When else part will be executed wrt loops?
+#Difference between del and None ?
+#Q.How to define multi-line String literals ?
+"""Q. Write a program to access each character of string in forward and backward direction 
+   by using while loop and for loop?"""
+"""Q.Counting substring in the given String ?
+      s="abcabcabcabcadda" count a,ab,"""
+"""Q.Replacing a string with another string:
+   s="Learning Python is very difficult"
+   o/p:Learning Python is very easy"""
+
+"""Q.Joining of Strings ?
+t=('sunny','bunny','chinny')
+o/p:sunny-bunny-chinny
+"""
+
+"""Q.
+l=['hyderabad','singapore','london','dubai']
+o/p:hyderabad:singapore:london:dubai
+"""
+"""s='learning Python is very Easy'
+o/p: LEARNING PYTHON IS VERY EASY
+o/p:learning python is very easy
+o/p:LEARNING pYTHON IS VERY eASY
+o/p:Learning Python Is Very Easy
+o/p:Learning python is very easy
+"""
+"""Q.Formatting the Strings ?
+name="john"
+salary=30000
+address="london"
+age=48
+o/p:john 's salary is 30000 and his age is 48 and he is from london
+"""
+
+"""Q.Program to reverse order of words 
+input:Enter Some String:Learning Python is very easy!!
+o/p: easy!!! very is Python Learning
+"""
+"""Q3. Program to reverse internal content of each word.
+input: web Software Solutions
+output:bew erawtfoS snoituloS
+"""
+"""Q4. Write a program to print characters at odd position and even position for the given 
+String?
+"""
+"""Q5. Program to merge characters of 2 strings into a single string by taking characters 
+alternatively.
+s1="ravi"
+s2="reja"
+output: rtaevjia
+"""
+
+"""Q6. Write a program to sort the characters of the string and first alphabet symbols 
+followed by numeric values
+input: B4A1D3
+Output: ABD134
+"""
+
+"""Q7. Write a program for the following requirement 
+input: a4b3c2
+output: aaaabbbcc
+"""
+
+"""Q9. Write a program to remove duplicate characters from the given input string?
+input: ABCDABBCDABBBCCCDDEEEF
+output: ABCDEF
+"""
+"""Q10. Write a program to find the number of occurrences of each character present in the 
+given String?
+input: ABCABCABBCDE
+output: A-3,B-4,C-3,D-1,E-1
+"""
+
+#list compression
+"""Qt] 
+words=["Balaiah","Nag","Venkatesh","Chiranjeevi"] 
+o/p:['B', 'N', 'V', 'C'] 
+"""
+
+"""
+Qt]
+num1=[100,200,300,400] 
+num2=[300,400,500,600]
+common elements present in num1 and num2 o/p:[300,400]
+num1 is not in num2 o/p:[100,200]
+"""
+"""
+QT]
+words="the quick brown fox jumps over the lazy dog" 
+o/p: ['the', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'] 
+"""
